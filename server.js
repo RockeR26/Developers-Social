d35381e0580c00166ec3aaf4e8668864aee40e71
unknown <messithiago300@gmail.com>
2020-09-20 19:39:30 +0530

    prod

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
+const path = require("path");
+const { read } = require("fs");
 
 //connecting to DB
 connectDB();
@@ -12,15 +14,20 @@ app.use(express.json({ extended: false }));
 // @route: GET /
 // @desc:SERVER HOME ROUTE
 // @acess:PUBLIC
-app.get("/", (req, res) => {
-  res.send("Im Server");
-});
 
 //routes of server
 app.use("/user", require("./routes/users"));
 app.use("/post", require("./routes/posts"));
 app.use("/profile", require("./routes/profiles"));
 app.use("/auth", require("./routes/auth"));
+
+//static assets in prod
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    read.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 //application listening on follwing port
 const PORT = process.env.PORT || 5000;
